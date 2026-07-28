078097d40e8dfc3eb60f46d3b13de87e620b012d
SpeedBroker <carlosaccs13@gmail.com>
2026-07-28 10:31:51 -0300

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -39,6 +39,7 @@ const GERENTES_AUTORIZADOS = {
   "barbosa7yui-pix11992617026":"Barbosa SP3",
   "fabio7ntz-pix11992617026":"Fabio SP3",
   "milene7ufs-pix11992617026":"Milene SP3",
+  "evelyn7out-pix11992617026":"Evelyn SP3",
 };
 
 function obterParametroUrl(nome) {
