0e10517e9170de0b3fc1c356301ed4214882b9d4
SpeedBroker <carlosaccs13@gmail.com>
2026-08-20 15:06:26 -0300

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -23,7 +23,7 @@ const GERENTES_AUTORIZADOS = {
   "adriano3gtn-11992617026": "Adriano SP2",
   "brunaluiza8lls-pix11992617026": "Bruna Luiza SP2",
   "daniel7ujk-pix11992617026": "Daniel SP@",
-  "ruiz7gny-pix11992617026": "Ruiz SP2",
+  "ruiz8hny-pix11992617026": "Ruiz SP2",
   "mizael7irf-pix11992617026": "Mizael SP2",
   "renato8cff-pix11992617026": "Renato SP2",
   "larissa7erg-pix11992617026": "Larissa SP2",
